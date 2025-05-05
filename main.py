c13702e4a633ce335b19e7b91ebf05f517fa0336
Naresh <phoenixarjun007@gmail.com>
2025-05-05 18:37:54 +0530

    solution

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from langchain_helper import get_few_shot_db_chain
+
+st.title("AtliQ T Shirts: Database Q&A 👕")
+
+question = st.text_input("Question: ")
+
+if question:
+    chain = get_few_shot_db_chain()
+    result = chain.invoke(question)
+
+    st.header("Answer")
+    st.write(result['result'])
